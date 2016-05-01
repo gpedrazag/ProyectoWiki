43f6f156fb97cd983bc239609e93f61ae9871ad6
admin <david_7956@hotmail.com>
2016-05-01 17:19:41 +0200

    Se completan los controladores

---
+++ wiki/WikiWeb/src/main/java/net/unipiloto/wiki/web/controllers/AssumptionController.java
@@ -0,0 +1,54 @@
+package net.unipiloto.wiki.web.controllers;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import net.unipiloto.wiki.web.transactions.AssumptionTransaction;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/assumption")
+public class AssumptionController {
+
+    @RequestMapping(value = "/insert", method = RequestMethod.POST)
+    public void insert(@RequestParam(value = "id") String id, @RequestParam(value = "description") String description, @RequestParam(value = "source") String source) {
+        try {
+            AssumptionTransaction.insert("assumption_" + id, description, source);
+        } catch (IOException | URISyntaxException ex) {
+            Logger.getLogger(ArtifactController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    @RequestMapping(value = "/update", method = RequestMethod.POST)
+    public void update(@RequestParam(value = "id") String id, @RequestParam(value = "description") String description, @RequestParam(value = "source") String source) {
+        try {
+            AssumptionTransaction.update("assumption_" + id, description, source);
+        } catch (IOException | URISyntaxException ex) {
+            Logger.getLogger(ArtifactController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    @RequestMapping(value = "/delete", method = RequestMethod.POST)
+    public void delete(@RequestParam(value = "id") String id) {
+        try {
+            AssumptionTransaction.delete("assumption_" + id);
+        } catch (IOException | URISyntaxException ex) {
+            Logger.getLogger(ArtifactController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    @RequestMapping(value = "/selectById", method = RequestMethod.POST)
+    public String selectById(@RequestParam(value = "id") String id) {
+        return AssumptionTransaction.selectById("assumption_" + id);
+    }
+
+    @RequestMapping(value = "/selectAll", method = RequestMethod.POST)
+    public String selectAll() {
+        return AssumptionTransaction.selectAll();
+    }
+
+}
---
+++ wiki/WikiWeb/src/main/java/net/unipiloto/wiki/web/controllers/FunctionalRequerimentController.java
@@ -0,0 +1,55 @@
+package net.unipiloto.wiki.web.controllers;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import net.unipiloto.wiki.web.transactions.FunctionalRequerimentTransaction;
+import org.openrdf.repository.Repository;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/functionalRequeriment")
+public class FunctionalRequerimentController {
+
+    @RequestMapping(value = "/insert", method = RequestMethod.POST)
+    public void insert(@RequestParam(value = "id") String id, @RequestParam(value = "name") String name, @RequestParam(value = "actor") String actor, @RequestParam(value = "description") String description, @RequestParam(value = "input") String input, @RequestParam(value = "output") String output) {
+        try {
+            FunctionalRequerimentTransaction.insert("functionalRequeriment_" + id, name, actor, description, input, output);
+        } catch (IOException | URISyntaxException ex) {
+            Logger.getLogger(ArtifactController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    @RequestMapping(value = "/update", method = RequestMethod.POST)
+    public void update(@RequestParam(value = "id") String id, @RequestParam(value = "name") String name, @RequestParam(value = "actor") String actor, @RequestParam(value = "description") String description, @RequestParam(value = "input") String input, @RequestParam(value = "output") String output) {
+        try {
+            FunctionalRequerimentTransaction.update("functionalRequeriment_" + id, name, actor, description, input, output);
+        } catch (IOException | URISyntaxException ex) {
+            Logger.getLogger(ArtifactController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    @RequestMapping(value = "/delete", method = RequestMethod.POST)
+    public void delete(@RequestParam(value = "id") String id) {
+        try {
+            FunctionalRequerimentTransaction.delete("functionalRequeriment_" + id);
+        } catch (IOException | URISyntaxException ex) {
+            Logger.getLogger(ArtifactController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    @RequestMapping(value = "/selectById", method = RequestMethod.POST)
+    public String selectById(@RequestParam(value = "id") String id) {
+        return FunctionalRequerimentTransaction.selectById("functionalRequeriment_" + id);
+    }
+
+    @RequestMapping(value = "/selectAll", method = RequestMethod.POST)
+    public String selectAll(@RequestParam(value = "id") String id, @RequestParam(value = "repository") Repository repository) {
+        return FunctionalRequerimentTransaction.selectAll("functionalRequeriment_" + id, repository);
+    }
+
+}
