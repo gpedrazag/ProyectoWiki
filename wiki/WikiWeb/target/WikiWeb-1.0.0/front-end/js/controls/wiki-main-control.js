04eeadfa49b8f085bfff1dd045de3ee61f6881ff
admin <david_7956@hotmail.com>
2016-05-31 13:17:49 -0500

    iltimos cambios con el index

---
+++ wiki/WikiWeb/target/WikiWeb-1.0.0/front-end/js/controls/wiki-main-control.js
@@ -0,0 +1,111 @@
+$(document).ready(function () {
+
+    $("#c-1").on("click", eventCreate);
+    $("#c-2").on("click", eventCreate);
+    $("#c-3").on("click", eventCreate);
+    $("#c-4").on("click", eventCreate);
+    $("#c-5").on("click", eventCreate);
+    $("#c-6").on("click", eventCreate);
+    $("#c-7").on("click", eventCreate);
+    $("#c-8").on("click", eventCreate);
+    $("#c-9").on("click", eventCreate);
+    $("#c-10").on("click", eventCreate);
+    $("#c-11").on("click", eventCreate);
+    $("#c-12").on("click", eventCreate);
+    $("#c-13").on("click", eventCreate);
+
+    $("#m-1").on("click", eventModify);
+    $("#m-2").on("click", eventModify);
+    $("#m-3").on("click", eventModify);
+    $("#m-4").on("click", eventModify);
+    $("#m-5").on("click", eventModify);
+    $("#m-6").on("click", eventModify);
+    $("#m-7").on("click", eventModify);
+    $("#m-8").on("click", eventModify);
+    $("#m-9").on("click", eventModify);
+    $("#m-10").on("click", eventModify);
+    $("#m-11").on("click", eventModify);
+    $("#m-12").on("click", eventModify);
+    $("#m-13").on("click", eventModify);
+
+    $("#d-1").on("click", eventDelete);
+    $("#d-2").on("click", eventDelete);
+    $("#d-3").on("click", eventDelete);
+    $("#d-4").on("click", eventDelete);
+    $("#d-5").on("click", eventDelete);
+    $("#d-6").on("click", eventDelete);
+    $("#d-7").on("click", eventDelete);
+    $("#d-8").on("click", eventDelete);
+    $("#d-9").on("click", eventDelete);
+    $("#d-10").on("click", eventDelete);
+    $("#d-11").on("click", eventDelete);
+    $("#d-12").on("click", eventDelete);
+    $("#d-13").on("click", eventDelete);
+    
+    //$("<div>").wikiConsultaGeneral();
+    $("<div>").wikiMainPage();
+
+    function eventCreate() {
+
+        var id = $(this).attr("id");
+        $("<div>").wikiCrearAlternativa(id);
+        $("<div>").wikiCrearArquitectura(id);
+        $("<div>").wikiCrearArtefacto(id);
+        $("<div>").wikiCrearAsunto(id);
+        $("<div>").wikiCrearAtrCalidad(id);
+        $("<div>").wikiCrearCriterio(id);
+        $("<div>").wikiCrearDecision(id);
+        $("<div>").wikiCrearEvaluacion(id);
+        $("<div>").wikiCrearRequerimientoFuncional(id);
+        $("<div>").wikiCrearResponsable(id);
+        $("<div>").wikiCrearRestriccion(id);
+        $("<div>").wikiCrearSolucion(id);
+        $("<div>").wikiCrearSuposicion(id);
+
+    }
+
+    function eventModify() {
+
+        var id = $(this).attr("id");
+        $("<div>").wikiModificarAlternativa(id);
+        $("<div>").wikiModificarArquitectura(id);
+        $("<div>").wikiModificarArtefacto(id);
+        $("<div>").wikiModificarAsunto(id);
+        $("<div>").wikiModificarAtrCalidad(id);
+        $("<div>").wikiModificarCriterio(id);
+        $("<div>").wikiModificarDecision(id);
+        $("<div>").wikiModificarEvaluacion(id);
+        $("<div>").wikiModificarRequerimientoFuncional(id);
+        $("<div>").wikiModificarResponsable(id);
+        $("<div>").wikiModificarRestriccion(id);
+        $("<div>").wikiModificarSolucion(id);
+        $("<div>").wikiModificarSuposicion(id);
+
+    }
+
+    function eventDelete() {
+        var id = $(this).attr("id");
+        $("<div>").wikiEliminarAlternativa(id);
+        $("<div>").wikiEliminarArquitectura(id);
+        $("<div>").wikiEliminarArtefacto(id);
+        $("<div>").wikiEliminarAsunto(id);
+        $("<div>").wikiEliminarAtributoDeCalidad(id);
+        $("<div>").wikiEliminarCriterio(id);
+        $("<div>").wikiEliminarDecision(id);
+        $("<div>").wikiEliminarEvaluacion(id);
+        $("<div>").wikiEliminarRequerimiento(id);
+        $("<div>").wikiEliminarResponsable(id);
+        $("<div>").wikiEliminarRestriccion(id);
+        $("<div>").wikiEliminarSolucion(id);
+        $("<div>").wikiEliminarSuposicion(id);
+
+    }
+
+});
+
+function search()
+{
+    
+}
+
+
